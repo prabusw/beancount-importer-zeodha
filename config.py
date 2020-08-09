e77b379b52761eb32555c6688fead908f5c1e74c
prabusw <69340977+prabusw@users.noreply.github.com>
2020-08-10 00:35:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ sys.path.append(os.path.dirname(__file__))
 # importers located in the importers directory
 from importers.icici import icici
 from importers.zerodha import zerodha
+# The 3722 can be the last four character of your ICICI savings account. By suitably changing you can import from any ICICI account.
+# All the six accounts i.e Assets:IN:Investment:ILFSSS etc must be declared already in your my.beancount file, to make use of this importer.
 
 CONFIG = [
     icici.IciciBankImporter('Assets:IN:ICICIBank:Savings', '3722'),
