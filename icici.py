07bad8fa347cb7b226995e69f64718322794b1d4
prabusw <69340977+prabusw@users.noreply.github.com>
2020-09-06 08:16:45 +0530

    V0.3
    V0.3 made changes to accept the headings as found in icici statement downloads as it is.
    In V0.2 made changes to automatically recognize credit and Debit transactions by changing sign based on importers-schwab.py script

--- icici.py
+++ icici.py
@@ -1,10 +1,12 @@
 """Importer for ICICI Bank, India.
 This script is heavily based on the script importers-chase.py  by Matt Terwilliger. 
 Original script can be found here https://gist.github.com/mterwill/7fdcc573dc1aa158648aacd4e33786e8
+In V0.2 made changes to automatically recognize credit and Debit transactions by changing sign based on importers-schwab.py script
+In V0.3 made changes to accept the headings as found in icici statement downloads as it is. 
 """
 __copyright__ = "Copyright (C) 2020  Prabu Anand K"
 __license__ = "GNU GPLv3"
-__Version__ = "0.1"
+__Version__ = "0.3"
 
 from beancount.core.number import D
 from beancount.ingest import importer
@@ -34,17 +36,32 @@ class IciciBankImporter(importer.ImporterProtocol):
 
         with open(f.name) as f:
             for index, row in enumerate(csv.DictReader(f)):
-                trans_date = parse(row['Posting Date']).date()
-                trans_desc = titlecase(row['Description'])
-                trans_amt  = row['Amount']
+                trans_date = parse(row['Value Date']).date()
+                trans_desc = titlecase(row['Transaction Remarks'])
+                #trans_amt  = row['Amount']
+                
+                if row['Withdrawal Amount (INR )'] != "0" :    # this is needed for ICICI. 
+                    trans_amt = float(row['Withdrawal Amount (INR )']) * -1.
+                elif row['Deposit Amount (INR )']:
+                    #trans_amt = float(row['Credit'].strip('$'))
+                    #left the above line to know about strip option 
+                    trans_amt = float(row['Deposit Amount (INR )'])
+                else:
+                    continue # 0 value transaction
+
+                trans_amt = '{:.2f}'.format(trans_amt)
+
+
+
+
                 meta = data.new_metadata(f.name, index)
 
                 txn = data.Transaction(
                     meta=meta,
                     date=trans_date,
                     flag=flags.FLAG_OKAY,
-                    payee=trans_desc,
-                    narration="",
+                    payee="",
+                    narration=trans_desc,
                     tags=set(),
                     links=set(),
                     postings=[],
