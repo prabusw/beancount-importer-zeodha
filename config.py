e2782d0b4d6b7ce6de3081f9ed231b98748642b5
prabusw <69340977+prabusw@users.noreply.github.com>
2020-09-07 11:16:00 +0530

    V0.2
    In V0.2 added information related to sbi

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
-"""Import configuration file for Zerodha, an Indian stock broker and  ICICI Bank, a bank in India .
+"""Import configuration file for Zerodha, an Indian stock broker, ICICI Bank, and SBI leading banks in India.
 This script is heavily based on the script config.py  by Matt Terwilliger. 
 Original script can be found here https://gist.github.com/mterwill/7fdcc573dc1aa158648aacd4e33786e8
+in V0.2 added information related to sbi
 """
 __copyright__ = "Copyright (C) 2020  Prabu Anand K"
 __license__ = "GNU GPLv3"
-__Version__ = "0.1"
+__Version__ = "0.2"
 
 import os, sys
 
@@ -15,11 +16,13 @@ sys.path.append(os.path.dirname(__file__))
 from importers.icici import icici
 from importers.zerodha import zerodha
 # The 3722 can be the last four character of your ICICI savings account. By suitably changing you can import from any ICICI account.
-# All the six accounts i.e Assets:IN:Investment:ILFSSS etc must be declared already in your my.beancount file, to make use of this importer.
+# The 3722 can be the last four character of your ICICI savings account. By suitably changing you can import from any ICICI account.
+# All the accounts must be declared already in your my.beancount file, to make use of this importer.
 
 CONFIG = [
     icici.IciciBankImporter('Assets:IN:ICICIBank:Savings', '3722'),
-
+    sbi.SBIImporter('Assets:IN:SBI:Savings', '3722'),
+    
     zerodha.ZerodhaImporter("INR",
                         "Assets:IN:Investment:Zerodha",
                         "Assets:IN:Investment:Zerodha:Cash",
